4cb5c62bcb553a5171b828ec87d9cae2dbc32f0e
Ste Creevy <steve.creevy@theoaklandgroup.co.uk>
2018-11-15 17:50:37 +0000

    Updated pykemon for alex review

---
+++ Python/Pykemon0.2.py
@@ -0,0 +1,28 @@
+import random
+secretNumber = random.randint(1, 20)
+pokemon_list = ['Bulbasaur','Ivysaur','Venusaur','Charmander','Charmeleon','Charizard','Squirtle','Wartortle','Blastoise','Caterpie','Metapod','Butterfree','Weedle','Kakuna','Beedrill','Pidgey','Pidgeotto','Pidgeot','Rattata','Raticate','Spearow','Fearow','Ekans','Arbok','Pikachu','Raichu','Sandshrew','Sandslash','Nidoran♀','Nidorina','Nidoqueen','Nidoran♂','Nidorino','Nidoking','Clefairy','Clefable','Vulpix','Ninetales','Jigglypuff','Wigglytuff','Zubat','Golbat','Oddish','Gloom','Vileplume','Paras','Parasect','Venonat','Venomoth','Diglett','Dugtrio','Meowth','Persian','Psyduck','Golduck','Mankey','Primeape','Growlithe','Arcanine','Poliwag','Poliwhirl','Poliwrath','Abra','Kadabra','Alakazam','Machop','Machoke','Machamp','Bellsprout','Weepinbell','Victreebel','Tentacool','Tentacruel','Geodude','Graveler','Golem','Ponyta','Rapidash','Slowpoke','Slowbro','Magnemite','Magneton',"Farfetch'd",'Doduo','Dodrio','Seel','Dewgong','Grimer','Muk','Shellder','Cloyster','Gastly','Haunter','Gengar','Onix','Drowzee','Hypno','Krabby','Kingler','Voltorb','Electrode','Exeggcute','Exeggutor','Cubone','Marowak','Hitmonlee','Hitmonchan','Lickitung','Koffing','Weezing','Rhyhorn','Rhydon','Chansey','Tangela','Kangaskhan','Horsea','Seadra','Goldeen','Seaking','Staryu','Starmie','Mr. Mime','Scyther','Jynx','Electabuzz','Magmar','Pinsir','Tauros','Magikarp','Gyarados','Lapras','Ditto','Eevee','Vaporeon','Jolteon','Flareon','Porygon','Omanyte','Omastar','Kabuto','Kabutops','Aerodactyl','Snorlax','Articuno','Zapdos','Moltres','Dratini','Dragonair','Dragonite','Mewtwo','Mew']
+r = random.choice(pokemon_list)
+pokemon = (r)
+print('Oh!... What is this?... \n Woah! \n A wild pokemon appeared out of nowhere! \n Its a....{0}! Do you want to try and catch it? Y/N'.format(pokemon))
+catch = str(input())
+if catch == 'Y' or 'y':
+        print('I am thinking of a number between 1 and 20.')
+        print('Guess it in 6 or less attempts and {0} is all yours!'.format(pokemon))
+elif catch == 'N' or 'n':
+        print('You try to flee.... \n it was close, but you got away safely!')
+for guessesTaken in range(1, 7):
+    print('Take a guess to catch {0}'.format(pokemon))
+    guess = int(input())
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break
+if guess == secretNumber:
+    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+    print('{0} has been captured and sent to the computer!'.format(pokemon))
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
+    print('Unfortunatley...{0} escaped...'.format(pokemon))
+
